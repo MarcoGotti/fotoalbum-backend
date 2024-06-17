b097a91262075acd48c8f8eb8d093f82927c69cd
MarcoGotti <mrc.gotti@gmail.com>
2024-06-17 13:36:58 +0000

    add category->photos show.blade

--- resources/views/admin/categories/show.blade.php
+++ resources/views/admin/categories/show.blade.php
@@ -7,12 +7,31 @@
             <h3>
                 Photos of <strong>{{ $category->name }}</strong>
             </h3>
-            <a class="btn btn-sm btn-secondary" href="{{ route('admin.photos.index') }}">Back to list</a>
+            <a class="btn btn-sm btn-secondary" href="{{ route('admin.categories.index') }}">Back to list</a>
         </div>
     </header>
 
     <div class="container">
         <div class="row">
+            @forelse ($category->photos as $photo)
+                <div class="col-2">
+                    <div class="card border border-5 border-secondary rounded">
+                        <a href="{{ route('admin.photos.show', $photo) }}">
+                            <img class="img-fluid" src="{{ $photo->upload }}" alt="no image">
+                        </a>
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $photo->title }}</h5>
+                        </div>
+                    </div>
+                </div>
+
+            @empty
+                <div class="col-12 mt-5 border border-warning border-1 text-center">
+                    <h4 class="text-warning">There are no photos related to category <strong>{{ $category->name }}</strong>.
+                        Go make some!
+                    </h4>
+                </div>
+            @endforelse
 
             <div class="col-2">
 
